197b57324562332aa1385a102b73c23b582052ed
myselfabhi <abhinavverma2024@gmail.com>
2024-08-09 04:19:13 +0530

    lint error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+module.exports = {
+    eslint: {
+      dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    },
+  }
 
 export default nextConfig;
